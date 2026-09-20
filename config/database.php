891cee6c4159b0b953a4ec6430161a69e611ab87
Khalil Zarei <zarei.khalil@gmail.com>
2026-09-20 23:40:38 +0330

    Update database.php

--- config/database.php
+++ config/database.php
@@ -3,14 +3,14 @@
 declare(strict_types=1);
 
 $host = "localhost";
-$user = "madahino_user";
-$password = "@65877856.com";
-$database = "madahino_madahi";
+$user = "";
+$password = "";
+$database = "";
 
 return [
     'host' => 'localhost',
     'port' => 3306,
-    'database' => 'madahino_madahi',
-    'username' => 'madahino_user',
-    'password' => '@65877856.com',
+    'database' => '',
+    'username' => '',
+    'password' => '',
 ];
